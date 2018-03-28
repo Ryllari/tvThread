2358c642d95083997314bec73b13d9603797a3cb
ryllari <ryllari.marques@lit.ifce.edu.br>
2018-03-28 08:34:12 -0300

    Criada a funcao de assistir e descansar Iserir JOptionPane para definir numero de hóspedes e criá-los

--- src/tvthread/Hospede.java
+++ src/tvthread/Hospede.java
@@ -1,7 +1,8 @@
 package tvthread;
 
-import java.util.Random;
+import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
+import java.util.Random;
 
 
 public class Hospede extends Thread {
@@ -11,15 +12,20 @@ public class Hospede extends Thread {
     private int ta; // tempo assistindo tv
     private int td; // tempo de descanso
 
+    static int assistindo;
+    static Semaphore mutex = new Semaphore(1);
+    static Semaphore tv = new Semaphore(1);
+    static Semaphore ttv = new Semaphore(1);
+    static int tvcanal = 0;
    
     Random escolha = new Random();
     
     // CONSTRUTOR
     public Hospede(int id, int canal, int ta1, int ta2, int td1, int td2){
         this.id = id;
-        this.canal = escolha.nextInt(canal);
-        this.ta = (escolha.nextInt(ta2) + ta1) * 1000;
-        this.td = (escolha.nextInt(td2) + td1) * 1000;    
+        this.canal = escolha.nextInt(canal) + 1;
+        this.ta = escolha.nextInt(ta2-ta1) + ta1;
+        this.td = escolha.nextInt(td2-td1) + td1;    
     }
     
     // FUNCOES GET
@@ -41,19 +47,48 @@ public class Hospede extends Thread {
                 
     // FAZENDO ALGO QUE CONSOME TEMPO
     public void doing(int time_to){
-        long t = System.currentTimeMillis() + time_to;
+        long t = System.currentTimeMillis() + (time_to * 1000);
             while(System.currentTimeMillis() < t){}
+    }
+    
+    public void assiste(){
+        
+        try {
+            mutex.acquire();
+        } catch (InterruptedException ex) {}
+        assistindo += 1;        
+        if (assistindo == 1){
+            try {
+                tv.acquire();  
+                tvcanal = this.getcanal();
+            } catch (InterruptedException ex) {}
+        }
+        System.out.println("Hospede "+this.getid()+" assistindo TV");
+        mutex.release();        
+    }
+    
+    public void descansa(){ // Hospede termina de assistir TV
+        try {
+            mutex.acquire();
+        } catch (InterruptedException ex) {}
+        assistindo -= 1;
+        System.out.println("Hospede "+this.getid()+" foi descansar");
+        if (assistindo == 0){
+            tvcanal = 0;
+            tv.release();
+        }
+        mutex.release();
     }
     
     @Override
     public void run (){
-        System.out.println("Hospede "+this.getid()+" diz: CHEGUEI!!!! CANAL="+this.getcanal()+"TA="+this.getta()+" TD="+this.gettd());
+        System.out.println("Hospede "+this.getid()+" diz: CHEGUEI!!!! CANAL= "+this.getcanal()+" TA="+this.getta()+" TD="+this.gettd());
         while(true){
-            System.out.println("Hospede "+this.getid()+" diz: VOU ASSISTIR");
-            this.doing(this.getta());
-            System.out.println("Hospede "+this.getid()+" diz: VOU DORMIR");
+            this.assiste();            
+            this.doing(this.getta());        
+            this.descansa();
             this.doing(this.gettd());
-            System.out.println("Hospede "+this.getid()+" diz: ACORDEI");
+            System.out.println("Hospede "+this.getid()+" diz: ACORDEI");           
         }
     }
 
--- src/tvthread/TvThread.java
+++ src/tvthread/TvThread.java
@@ -1,22 +1,25 @@
 package tvthread;
 
-public class TvThread {
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
+public class TvThread {
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        int h = 5;
-        int n = 44;
-        int ta1 = 10;
-        int ta2 = 30;
-        int td1 = 15;
-        int td2 = 40;
+        int h = Integer.parseInt(JOptionPane.showInputDialog("Bem-vindo ao Hotel IDK!\nQual é o número de hópedes?"));
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Qual é a quantidade de canais?"));
+        JOptionPane.showMessageDialog(null ,"Vamos organizar as coisas agora!");
+        int ta1 = Integer.parseInt(JOptionPane.showInputDialog("Qual é o tempo mínimo assistindo?"));
+        int ta2 = Integer.parseInt(JOptionPane.showInputDialog("Qual é o tempo máximo assistindo?"));
+        int td1 = Integer.parseInt(JOptionPane.showInputDialog("Qual é o tempo mínimo descansando?"));
+        int td2 = Integer.parseInt(JOptionPane.showInputDialog("Qual é o tempo máximo descansando?"));
         
-        Hospede h1 = new Hospede(1, n, ta1, ta2, td1, td2);
-        Hospede h2 = new Hospede(2, n, ta1, ta2, td1, td2);
-        Hospede h3 = new Hospede(3, n, ta1, ta2, td1, td2); 
-        h1.start();
-        h2.start();
-        h3.start();
+        for (int i = 0; i< h; i++){
+            Hospede hos = new Hospede(i, n, ta1, ta2, td1, td2);
+            hos.start();
+        }
+
 
     }
     
